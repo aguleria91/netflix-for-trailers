f4e9042ff6be4887e0d6e9866a9533aae09647f3
Aditya Guleria <aditya@koresoft.in>
2022-06-01 15:57:11 +0530

    readme updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,11 @@ const Home = ({
   if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
